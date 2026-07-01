df619e043512105cc21d056ffb2dc58283fddb08
Ufuk Topalan <double123416@gmail.com>
2026-07-01 13:50:00 +0300

    Add metadata comments to plotDiversity.R
    Added author information and repository link.

--- scripts/plotDiversity.R
+++ scripts/plotDiversity.R
@@ -1,3 +1,8 @@
+# Authors: Ufuk Topalan, Ismail Kudret Saglam
+# Written for: Genome-Wide Analysis Reveals Altitude-Associated
+#              Divergence in a Color-Polymorphic Insect (Topalan & Sağlam, 2026)
+# Repository: https://github.com/UfukTopalan/Genome-Wide-Analysis-Pipeline-for-Color-Polymorphic-Insect
+# ------------------------------------------------------------------
 library(ggplot2)
 library(ggsignif)
 library(psych)
